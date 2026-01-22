8f4640e406521ae1b3fa1999422770a1e2955859
esther <esthersant.edu@gmail.com>
2026-01-22 14:01:08 -0300

    Roman to Integer

--- easy/Solution.java
+++ easy/Solution.java
@@ -1,12 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
-    public boolean isPalindrome(int x) {
-        if (x < 0) return false;
-        int original = x;
-        int rev = 0;
-        while (x != 0) {
-            rev = rev * 10 + x % 10;
-            x /= 10;
+    public int romanToInt(String s) {
+            Map<Character, Integer> romanMap = new HashMap<>();
+            romanMap.put('I', 1);
+            romanMap.put('V', 5);
+            romanMap.put('X', 10);
+            romanMap.put('L', 50);
+            romanMap.put('C', 100);
+            romanMap.put('D', 500);
+            romanMap.put('M', 1000);
+
+            int total = 0;
+            int previousValue = 0;
+
+            for (char c : s.toCharArray()){
+                int currentValue = romanMap.get(c);
+                if (currentValue > previousValue){
+                    total += currentValue - (2 * previousValue);
+                } else {
+                    total += currentValue;
+                }
+                previousValue = currentValue;
+            }
+            return total;
         }
-        return original == rev;
-    } 
-}
+    }
