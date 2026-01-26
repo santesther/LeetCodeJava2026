05ecf91d9f48879cf89124172a3ae0f5a20435f4
esther <esthersant.edu@gmail.com>
2026-01-26 13:29:57 -0300

    Remove Duplicates from Sorted Array

--- easy/Solution.java
+++ easy/Solution.java
@@ -1,18 +1,13 @@
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode preHead = new ListNode(0);
-        ListNode last = preHead;
-        while (list1 != null && list2 != null){
-            if (list1.val > list2.val){
-                last.next = list2;
-                list2 = list2.next;
-            } else {
-                last.next = list1;
-                list1 = list1.next;
+    public int removeDuplicates(int[] nums) {
+        int uniqueNumbers=1;
+
+        for (int i=1; i<nums.length; i++){
+            if (nums[i] != nums[i-1]){
+                nums[uniqueNumbers] = nums[i];
+                uniqueNumbers++;
             }
-            last = last.next;
         }
-        last.next = list1 == null ? list2 : list1;
-        return preHead.next;
+        return uniqueNumbers;
     }
 }
